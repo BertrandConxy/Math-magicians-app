991105d14004e1c6feb6c042c465621dd7d9f012
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-02-15 16:18:10 +0200

    imported necessary files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,24 +6,24 @@ export default class Calculator extends Component {
     return (
       <div className="Calc-container">
         <div className="display-result">0</div>
-        <div className="clear pad">AC</div>
-        <div className="sign pad">+/-</div>
-        <div className="modular pad">%</div>
-        <div className="div operator">/</div>
-        <div className="seven pad">7</div>
-        <div className="eight pad">8</div>
-        <div className="nine pad">9</div>
-        <div className="mult operator">x</div>
-        <div className="four pad">4</div>
-        <div className="five pad">5</div>
-        <div className="six pad">6</div>
-        <div className="sub operator">-</div>
-        <div className="one pad">1</div>
-        <div className="two pad">2</div>
-        <div className="three pad">3</div>
-        <div className="plus operator">+</div>
-        <div className="zero pad">0</div>
-        <div className="equal operator">=</div>
+        <button type="button" className="clear pad">AC</button>
+        <button type="button" className="sign pad">+/-</button>
+        <button type="button" className="modular pad">%</button>
+        <button type="button" className="div operator">/</button>
+        <button type="button" className="seven pad">7</button>
+        <button type="button" className="eight pad">8</button>
+        <button type="button" className="nine pad">9</button>
+        <button type="button" className="mult operator">x</button>
+        <button type="button" className="four pad">4</button>
+        <button type="button" className="five pad">5</button>
+        <button type="button" className="six pad">6</button>
+        <button type="button" className="sub operator">-</button>
+        <button type="button" className="one pad">1</button>
+        <button type="button" className="two pad">2</button>
+        <button type="button" className="three pad">3</button>
+        <button type="button" className="plus operator">+</button>
+        <button type="button" className="zero pad">0</button>
+        <button type="button" className="equal operator">=</button>
       </div>
     );
   }
