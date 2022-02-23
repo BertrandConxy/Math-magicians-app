bf8158d374b6a0471e1e238ad66bebc2aefad28b
zairdon20 <josphat2020@gmail.com>
2022-02-23 16:04:20 +0200

    Add the test for calculator.test.js file

--- src/components/logic/calculate.test.js
+++ src/components/logic/calculate.test.js
@@ -15,7 +15,58 @@ describe('calculator works properly', () => {
         const {total} = calculate(obj, '=');
         expect(total).toEqual('12');
     })
-    test('subration operator works properly' ()=> {
-        
-    });
-}) 
+
+    test('Tests the substration operation', () => {
+        obj= {
+          total: '12',
+          next: '6',
+          operation: '-',
+        };
+    
+        const { total } = calculate(obj, '=');
+        expect(total).toBe('6');
+      })
+      test('Tests the division operation', () => {
+        obj= {
+          total: '12',
+          next: '6',
+          operation: '÷',
+        };
+    
+        const { total } = calculate(obj, '=');
+        expect(total).toBe('2');
+      })
+      test('Tests the multiplication operation', () => {
+        obj= {
+          total: '12',
+          next: '6',
+          operation: 'x',
+        };
+    
+        const { total } = calculate(obj, '=');
+        expect(total).toBe('72');
+      })
+      test('Tests modulus operation', () => {
+        obj= {
+          total: '12',
+          next: '6',
+          operation: '%',
+        };
+    
+        const { total } = calculate(obj, '=');
+        expect(total).toBe('0');
+      })
+      test('Tests reset (AC) button', () => {
+        obj= {
+          total: '12',
+          next: '6',
+          operation: 'x',
+        };
+    
+        expect(calculate(obj, 'AC')).toEqual({
+          total: null,
+          next: null,
+          operation: null,
+        });
+      });
+    })
